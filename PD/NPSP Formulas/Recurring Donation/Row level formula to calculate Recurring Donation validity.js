50a2a45254eed367a7e098baa8bc63ba05aea981
RBA <ribena75@pm.me>
2025-03-04 15:03:38 +0000

    swapped logic around and works now

--- PD/NPSP Formulas/Recurring Donation/Row level formula to calculate Recurring Donation validity.js
+++ PD/NPSP Formulas/Recurring Donation/Row level formula to calculate Recurring Donation validity.js
@@ -1,8 +1,8 @@
 IF(
   AND(
-    npe03__Recurring_Donation__c.npe03__Paid_Amount__c > 0,
-    TEXT(npe03__Recurring_Donation__c.npsp__Status__c) <> "Closed"
+    npe03__Recurring_Donation__c.npe03__Paid_Amount__c = 0,
+    TEXT(npe03__Recurring_Donation__c.npsp__Status__c) = "Closed"
   ),
-  "Valid",
-  "Invalid"
+  "Invalid",
+  "Valid"
 )
