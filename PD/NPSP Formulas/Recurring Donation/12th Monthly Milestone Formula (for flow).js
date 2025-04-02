7b16225c83a16a7179822e661f679e0afd77b07d
RBA <ribena75@pm.me>
2025-04-02 10:26:34 +0100

    added line to make sure RD is still active

--- PD/NPSP Formulas/Recurring Donation/12th Monthly Milestone Formula (for flow).js
+++ PD/NPSP Formulas/Recurring Donation/12th Monthly Milestone Formula (for flow).js
@@ -1,4 +1,5 @@
 AND(
   npe03__Total_Paid_Installments__c > 0,
-  MOD(npe03__Total_Paid_Installments__c, 12) = 0
+  MOD(npe03__Total_Paid_Installments__c, 12) = 0,
+  TEXT({!$Record.npsp__Status__c}) = 'Active'
 )
