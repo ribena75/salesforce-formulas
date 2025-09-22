c5e1a0689586377e4a4c28932d4f803be09536da
RBA <ribena75@pm.me>
2025-09-22 10:48:31 +0100

    update

--- PD/NPSP Formulas/Recurring Donation/Row-level formula for exceptional giving report.js
+++ PD/NPSP Formulas/Recurring Donation/Row-level formula for exceptional giving report.js
@@ -2,10 +2,10 @@ IF(
   Contact.npo02__AverageAmount__c = 0,
   "",
   IF(
-    OCR_Opportunity.Amount >= (3 * Contact.npo02__AverageAmount__c),
+    Contact.npo02__LastOppAmount__c >= (3 * Contact.npo02__AverageAmount__c),
     "3x+ Average Gift",
     IF(
-      OCR_Opportunity.Amount >= (2 * Contact.npo02__AverageAmount__c),
+      Contact.npo02__LastOppAmount__c >= (2 * Contact.npo02__AverageAmount__c),
       "2x Average Gift",
       ""
     )
