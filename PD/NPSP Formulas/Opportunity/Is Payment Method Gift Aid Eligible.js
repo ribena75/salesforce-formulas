2aa8c60873775729845049523456db4021184a7f
RBA <ribena75@pm.me>
2025-10-03 09:35:39 +0100

    Formula field on opportunity to define which payment types are eligible for Gift Aid

---
+++ PD/NPSP Formulas/Opportunity/Is Payment Method Gift Aid Eligible.js
@@ -0,0 +1,15 @@
+OR(
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Access PaySuite Direct Debit'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'BACS'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Cash'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Cheque'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'CreditCard'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Direct Debit'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Facebook'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'PayPal'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Standing Order'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Stripe'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Stripe Direct Debit'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'Rapidata'),
+ISPICKVAL(npsp4hub__Payment_Method__c, 'iATS - Offline')
+)
