7fd47f3fc1b3b29bf515e9df9dd15744abf6cb83
RBA <mail@ribena75.me>
2026-01-19 12:40:02 +0000

    Add condition to 12th Monthly Milestone Formula to check Do_Not_Acknowledge__c

--- PD/Salesforce NPSP Formulas/Recurring Donation/12th Monthly Milestone Formula (for flow).js
+++ PD/Salesforce NPSP Formulas/Recurring Donation/12th Monthly Milestone Formula (for flow).js
@@ -1,5 +1,6 @@
 AND(
   npe03__Total_Paid_Installments__c > 0,
   MOD(npe03__Total_Paid_Installments__c, 12) = 0,
-  TEXT({!$Record.npsp__Status__c}) = 'Active'
+  NOT(Do_Not_Acknowledge__c),
+  TEXT(npsp__Status__c) = 'Active'
 )
