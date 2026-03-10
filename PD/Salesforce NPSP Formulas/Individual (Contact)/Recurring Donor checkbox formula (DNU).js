8fb29747c0545d09fb255f99dfa1a0fc6e86fcc2
ribena75 <mail@ribena75.me>
2026-03-10 11:59:53 +0000

    Add Recurring Donor checkbox formula (DNU) to handle active recurring donations

--- PD/Salesforce NPSP Formulas/Individual (Contact)/Recurring Donor checkbox formula (DNU).js
+++ PD/Salesforce NPSP Formulas/Individual (Contact)/Recurring Donor checkbox formula (DNU).js
@@ -1,3 +1,4 @@
+// Cannot use this as field name is already in use and I cannot change type to that of a formula; find workaround for now 
 IF(
    AND(
       NOT(ISBLANK(Active_Recurring_Donations__c)),
