0d4d0b8b9ff0b12e4f5e206e1b0d345ca93bf66a
RBA <mail@ribena75.me>
2026-04-16 16:07:53 +0100

    Fix UTM Campaign value formula to reference Opportunity object for consistency

--- PD/Salesforce NPSP Formulas/Reports/Row level formula to clean UTM Campaign value.js
+++ PD/Salesforce NPSP Formulas/Reports/Row level formula to clean UTM Campaign value.js
@@ -1,9 +1,9 @@
 IF(
-  ISERROR(FIND("-", UTM_Campaign__c)),
-  UTM_Campaign__c,
+  ISERROR(FIND("-", Opportunity.UTM_Campaign__c)),
+  Opportunity.UTM_Campaign__c,
   IF(
-    LEN(LEFT(UTM_Campaign__c, FIND("-", UTM_Campaign__c) - 1)) >= 8,
-    MID(UTM_Campaign__c, FIND("-", UTM_Campaign__c) + 1, 999),
-    UTM_Campaign__c
+    LEN(LEFT(Opportunity.UTM_Campaign__c, FIND("-", Opportunity.UTM_Campaign__c) - 1)) >= 8,
+    MID(Opportunity.UTM_Campaign__c, FIND("-", Opportunity.UTM_Campaign__c) + 1, 999),
+    Opportunity.UTM_Campaign__c
   )
 )
