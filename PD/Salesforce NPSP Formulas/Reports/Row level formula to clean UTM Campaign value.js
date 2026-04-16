60371734342961584f11d1084a96f7372e1f7159
RBA <mail@ribena75.me>
2026-04-16 16:10:01 +0100

    Fix error handling in UTM Campaign value formula to improve robustness

--- PD/Salesforce NPSP Formulas/Reports/Row level formula to clean UTM Campaign value.js
+++ PD/Salesforce NPSP Formulas/Reports/Row level formula to clean UTM Campaign value.js
@@ -1,5 +1,5 @@
 IF(
-  ISERROR(FIND("-", Opportunity.UTM_Campaign__c)),
+  FIND("-", Opportunity.UTM_Campaign__c) = 0,
   Opportunity.UTM_Campaign__c,
   IF(
     LEN(LEFT(Opportunity.UTM_Campaign__c, FIND("-", Opportunity.UTM_Campaign__c) - 1)) >= 8,
