5383c27078ef3ecde32cd5c7b613029988772ede
ribena75 <mail@ribena75.me>
2026-04-28 15:42:58 +0100

    update

--- PD/Salesforce NPSP Formulas/Individual (Contact)/Household Mailing Address.js
+++
@@ -1,9 +0,0 @@
-//Managed formula - cannot be updated//
-
-IF(npe01__Organization_Type__c == 'Household Account',
-
-Account.BillingStreet & BR() &
-Account.BillingCity & IF(ISBLANK(Account.BillingCity), "", ", ") & Account.BillingState & " " & Account.BillingPostalCode
-& IF(ISBLANK(Account.BillingCountry), "", BR()& Account.BillingCountry),
-
-npo02__Household__r.npo02__Formula_MailingAddress__c)
