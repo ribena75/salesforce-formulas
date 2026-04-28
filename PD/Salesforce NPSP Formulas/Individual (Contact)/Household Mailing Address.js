04232ea0593b42e6590f662cb0b450e0aae507c1
ribena75 <mail@ribena75.me>
2026-04-28 13:23:16 +0100

    Add formula for Household Mailing Address based on Organization Type

---
+++ PD/Salesforce NPSP Formulas/Individual (Contact)/Household Mailing Address.js
@@ -0,0 +1,7 @@
+IF(npe01__Organization_Type__c == 'Household Account',
+
+Account.BillingStreet & BR() &
+Account.BillingCity & IF(ISBLANK(Account.BillingCity), "", ", ") & Account.BillingState & " " & Account.BillingPostalCode
+& IF(ISBLANK(Account.BillingCountry), "", BR()& Account.BillingCountry),
+
+npo02__Household__r.npo02__Formula_MailingAddress__c)
