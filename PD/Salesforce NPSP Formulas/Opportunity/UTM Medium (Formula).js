a37c2c882f61c19404c99090530fd5a769abd638
RBA <mail@ribena75.me>
2026-05-07 16:44:36 +0100

    fix: update UTM Medium and UTM Source formulas for consistency and accuracy

--- PD/Salesforce NPSP Formulas/Opportunity/UTM Medium (Formula).js
+++ PD/Salesforce NPSP Formulas/Opportunity/UTM Medium (Formula).js
@@ -2,14 +2,14 @@ IF(
   RecordType.Name = "Merchandise Sales", "Merch",
   IF(
     ISBLANK(UTM_Medium__c),
-    "Untracked",
+    "Other",
     CASE(
       UTM_Medium__c,
       "cpc", "Google",
       "email", "Email",
       "paid", "Meta",
       "paid-social", "Meta",
-      "Untracked"
+      "Other"
     )
   )
 )
