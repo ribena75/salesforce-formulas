a608c1f7a792bab3929612c9af2bb4fe64c73bed
RBA <mail@ribena75.me>
2026-05-07 13:17:25 +0100

    updated to include balnk values as untracked also

--- PD/Salesforce NPSP Formulas/Opportunity/utm_medium_formula.js
+++ PD/Salesforce NPSP Formulas/Opportunity/utm_medium_formula.js
@@ -1,11 +1,15 @@
 IF(
   RecordType.Name = "Merchandise Sales", "merch",
-  CASE(
-    UTM_Medium__c,
-    "cpc", "grant",
-    "email", "email",
-    "paid", "ads",
-    "paid-social", "ads",
-    "untracked"
+  IF(
+    ISBLANK(UTM_Medium__c),
+    "untracked",
+    CASE(
+      UTM_Medium__c,
+      "cpc", "grant",
+      "email", "email",
+      "paid", "ads",
+      "paid-social", "ads",
+      "untracked"
+    )
   )
 )
