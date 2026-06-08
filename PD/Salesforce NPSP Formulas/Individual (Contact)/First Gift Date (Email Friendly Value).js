77983ad04cf7d354aa6fb5aebf78cb20d19e49ac
RBA <mail@ribena75.me>
2026-06-08 16:33:25 +0100

    feat: add First Gift Date formula for email-friendly date representation

---
+++ PD/Salesforce NPSP Formulas/Individual (Contact)/First Gift Date (Email Friendly Value).js
@@ -0,0 +1,16 @@
+CASE(
+    MONTH(npo02__FirstCloseDate__c),
+    1, "January",
+    2, "February",
+    3, "March",
+    4, "April",
+    5, "May",
+    6, "June",
+    7, "July",
+    8, "August",
+    9, "September",
+    10, "October",
+    11, "November",
+    12, "December",
+    ""
+) & " " & TEXT(YEAR(npo02__FirstCloseDate__c))
