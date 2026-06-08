3bc885938d5018c823f98ed9eaf066a340f325c1
RBA <mail@ribena75.me>
2026-06-08 16:03:33 +0100

    feat: enhance Total Gifts formula for precise currency formatting

--- PD/Salesforce NPSP Formulas/Individual (Contact)/Total Gifts (Email Friendly Value).js
+++ PD/Salesforce NPSP Formulas/Individual (Contact)/Total Gifts (Email Friendly Value).js
@@ -4,4 +4,12 @@ CASE(
   "USD", "$",
   ""
 ) &
-TEXT(npo02__TotalOppAmount__c)
+TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2))) &
+"." &
+RIGHT(
+  "00" &
+  TEXT(
+    MOD(FLOOR(ROUND(npo02__TotalOppAmount__c, 2) * 100), 100)
+  ),
+  2
+)
