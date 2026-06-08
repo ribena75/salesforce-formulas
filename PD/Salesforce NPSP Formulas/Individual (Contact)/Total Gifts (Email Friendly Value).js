69e982aeefbce29104e192aeb8e629dc03e0a727
RBA <mail@ribena75.me>
2026-06-08 16:29:06 +0100

    feat: improve Total Gifts formula for enhanced currency representation and readability

--- PD/Salesforce NPSP Formulas/Individual (Contact)/Total Gifts (Email Friendly Value).js
+++ PD/Salesforce NPSP Formulas/Individual (Contact)/Total Gifts (Email Friendly Value).js
@@ -3,13 +3,31 @@ CASE(
   "GBP", "£",
   "USD", "$",
   ""
-) &
-TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2))) &
-"." &
-RIGHT(
-  "00" &
-  TEXT(
-    MOD(FLOOR(ROUND(npo02__TotalOppAmount__c, 2) * 100), 100)
-  ),
-  2
+)
+&
+IF(
+  ROUND(npo02__TotalOppAmount__c, 2) >= 1000000,
+  TEXT(ROUND(npo02__TotalOppAmount__c / 1000000, 1)) & "m",
+  IF(
+    ROUND(npo02__TotalOppAmount__c, 2) >= 10000,
+    TEXT(ROUND(npo02__TotalOppAmount__c / 1000, 1)) & "k",
+    IF(
+      LEN(TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2)))) > 3,
+      LEFT(
+        TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2))),
+        LEN(TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2)))) - 3
+      ) & "," &
+      RIGHT(TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2))), 3),
+      TEXT(FLOOR(ROUND(npo02__TotalOppAmount__c, 2)))
+    )
+    &
+    "." &
+    RIGHT(
+      "00" &
+      TEXT(
+        MOD(FLOOR(ROUND(npo02__TotalOppAmount__c, 2) * 100), 100)
+      ),
+      2
+    )
+  )
 )
