d80660988d85613e37e489721724fd07761e9a70
RBA <mail@ribena75.me>
2026-06-29 11:42:50 +0100

    feat: update Gift Aid Amount formula to include total amount for enhanced currency representation

--- PD/Salesforce NPSP Formulas/Opportunity/Gift Aid Amount (Email Friendly Value).js
+++ PD/Salesforce NPSP Formulas/Opportunity/Gift Aid Amount (Email Friendly Value).js
@@ -6,26 +6,26 @@ CASE(
 )
 &
 IF(
-  ROUND(gcga__Gift_Aid_Amount__c, 2) >= 1000000,
-  TEXT(ROUND(gcga__Gift_Aid_Amount__c / 1000000, 1)) & "m",
+  ROUND(Amount + gcga__Gift_Aid_Amount__c, 2) >= 1000000,
+  TEXT(ROUND(Amount + gcga__Gift_Aid_Amount__c / 1000000, 1)) & "m",
   IF(
-    ROUND(gcga__Gift_Aid_Amount__c, 2) >= 10000,
-    TEXT(ROUND(gcga__Gift_Aid_Amount__c / 1000, 1)) & "k",
+    ROUND(Amount + gcga__Gift_Aid_Amount__c, 2) >= 10000,
+    TEXT(ROUND(Amount + gcga__Gift_Aid_Amount__c / 1000, 1)) & "k",
     IF(
-      LEN(TEXT(FLOOR(ROUND(gcga__Gift_Aid_Amount__c, 2)))) > 3,
+      LEN(TEXT(FLOOR(ROUND(Amount + gcga__Gift_Aid_Amount__c, 2)))) > 3,
       LEFT(
-        TEXT(FLOOR(ROUND(gcga__Gift_Aid_Amount__c, 2))),
-        LEN(TEXT(FLOOR(ROUND(gcga__Gift_Aid_Amount__c, 2)))) - 3
+        TEXT(FLOOR(ROUND(Amount + gcga__Gift_Aid_Amount__c, 2))),
+        LEN(TEXT(FLOOR(ROUND(Amount + gcga__Gift_Aid_Amount__c, 2)))) - 3
       ) & "," &
-      RIGHT(TEXT(FLOOR(ROUND(gcga__Gift_Aid_Amount__c, 2))), 3),
-      TEXT(FLOOR(ROUND(gcga__Gift_Aid_Amount__c, 2)))
+      RIGHT(TEXT(FLOOR(ROUND(Amount + gcga__Gift_Aid_Amount__c, 2))), 3),
+      TEXT(FLOOR(ROUND(Amount + gcga__Gift_Aid_Amount__c, 2)))
     )
     &
     "." &
     RIGHT(
       "00" &
       TEXT(
-        MOD(FLOOR(ROUND(gcga__Gift_Aid_Amount__c, 2) * 100), 100)
+        MOD(FLOOR(ROUND(Amount + gcga__Gift_Aid_Amount__c, 2) * 100), 100)
       ),
       2
     )
