34f4cbeba8d9b4730515d924c1bff87649350633
RBA <mail@ribena75.me>
2026-09-14 15:13:12 +0100

    feat: restructure UTM Medium formula to include FundraiseUpId check and Merchandise Sales condition

--- PD/Salesforce NPSP Formulas/Opportunity/UTM Medium (Formula).js
+++ PD/Salesforce NPSP Formulas/Opportunity/UTM Medium (Formula).js
@@ -1,12 +1,18 @@
 IF(
-    ISBLANK(UTM_Medium__c),
-    "Other",
-    CASE(
-      UTM_Medium__c,
-      "cpc", "Google",
-      "email", "Email",
-      "paid", "Meta",
-      "paid-social", "Meta",
-      "Website"
+  ISBLANK(FundraiseUpId__c), "n/a",
+  IF(
+    RecordType.Name = "Merchandise Sales", "Merch",
+    IF(
+      ISBLANK(UTM_Medium__c),
+      "Other",
+      CASE(
+        UTM_Medium__c,
+        "cpc", "Google",
+        "email", "Email",
+        "paid", "Meta",
+        "paid-social", "Meta",
+        "Website"
+      )
     )
   )
+)
